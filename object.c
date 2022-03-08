6b9c02404eae81234bce9e4b0fa0a79090cf8f24
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2022-03-09 02:03:31 +1100

    Switched integer datatype to that compatible with Saturn

--- object.c
+++ object.c
@@ -1,6 +1,6 @@
 #include "object.h"
 
-OBJECT *object_allocate(int p_count) {
+OBJECT *object_allocate(int32_t p_count) {
 	// Calculate memory allocation size
 	p_count *= sizeof(POLYGON_T);
 	
--- object.h
+++ object.h
@@ -4,7 +4,7 @@
 
 // Datatype for points in space
 typedef struct vec3 {
-	int x, y, z;
+	int32_t x, y, z;
 } VEC3;
 
 // Datatype for a single polygon
@@ -12,11 +12,11 @@ typedef VEC3 POLYGON_T[3];
 
 // Datatype for object
 typedef struct object {
-	int p_count; // Size of array
+	int32_t p_count; // Size of array
 	POLYGON_T *polygons;
 	
 	// Origin point for the group of polygons
 	VEC3 origin;
 } OBJECT;
 
-OBJECT *object_allocate(int p_count);
+OBJECT *object_allocate(int32_t p_count);
